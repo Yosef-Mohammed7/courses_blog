2bcec25137838fd124b8d65deafafb34bc197531
Yosef-Mohammed7 <yoyo4822@gmail.com>
2020-10-11 01:08:11 +0300

    ptoject

---
+++ courses/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from django.db.models.signals import post_save
+
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
     'crispy_forms',
+    'courses',
 
 ]
 
